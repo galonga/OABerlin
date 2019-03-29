b063c749f8c61622010ee9356a71548bc60ea83e
Timo Arndt <office@visit-timo.de>
2019-03-29 13:27:50 +0100

    Initial commit

---
+++ app/src/main/java/de/galonga/ordnungsamtonline/clients/HybridWebViewClient.kt
@@ -0,0 +1,8 @@
+package de.galonga.ordnungsamtonline.clients
+
+import android.webkit.WebViewClient
+import de.galonga.ordnungsamtonline.MainActivity
+
+class HybridWebViewClient constructor(private val mainActivity: MainActivity) : WebViewClient() {
+
+}
